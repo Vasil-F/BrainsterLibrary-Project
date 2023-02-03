d698cac35ef642a4719c25513cc7e3ed00ec28f9
Vasil <filipovski.v@hotmail.com>
2023-02-03 22:04:57 +0100

    Added comments php file for comment functions

---
+++ comments/allComments.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once __DIR__ . '/../db/db.php';
+
+$sql = 'SELECT * FROM comments ';
+
+// SELECT books.*, authors.*, categories.title as cat_title FROM books JOIN authors ON books.author_id = authors.id JOIN categories ON books.category_id = categories.id;
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$data = $stmt->fetchAll();
+
+echo json_encode($data);
